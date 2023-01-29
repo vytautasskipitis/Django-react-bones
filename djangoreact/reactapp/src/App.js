46f60c0f3ccac1d03a8083e3e61368fe3f3118bc
VytautasSkipitis <87714408+vytautasskipitis@users.noreply.github.com>
2023-01-30 01:01:33 +0200

    Initial commit

---
+++ djangoreact/reactapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  console.log('asdasd')
+  return (
+    <div className="App">
+      hello asdasdasd
+    </div>
+  );
+}
+
+export default App;
